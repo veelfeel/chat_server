e9add81dfe44a2929906d6b339f4372a77f825a0
viktorfilmanovich <film.viktor@yandex.ru>
2023-07-28 21:19:26 +0300

    ...

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ wss.on("connection", function connection(ws) {
 
   ws.on("message", (data) => {
     // console.log(JSON.parse(data));
+
     wss.clients.forEach((client) => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(JSON.stringify(JSON.parse(data)));
