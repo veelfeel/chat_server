e238a0eb6be6d09059cef5a0cabfee853ab06a63
viktorfilmanovich <film.viktor@yandex.ru>
2023-07-29 20:48:17 +0300

    .

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
+import express from "express";
 import WebSocket, { WebSocketServer } from "ws";
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+app.use("/uploads", express.static(__dirname + "/uploads"));
+
+app.listen(4040, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log("server running at port 4040");
+});
 
 const wss = new WebSocketServer({ port: 8080 });
 
@@ -6,11 +24,25 @@ wss.on("connection", function connection(ws) {
   console.log("new client connected");
 
   ws.on("message", (data) => {
-    // console.log(JSON.parse(data));
+    const messageData = JSON.parse(data);
+    const { file } = messageData;
+    if (file) {
+      const parts = file.name.split(".");
+      const ext = parts[parts.length - 1];
+      const filename = Date.now() + "." + ext;
+      if (!fs.existsSync("uploads")) {
+        fs.mkdirSync("uploads");
+      }
+      const path = __dirname + "/uploads/" + filename;
+      const bufferData = new Buffer.from(file.data.split(",")[1], "base64");
+      fs.writeFile(path, bufferData, () => {
+        console.log("file saved:" + path);
+      });
+    }
 
     wss.clients.forEach((client) => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify(JSON.parse(data)));
+        client.send(JSON.stringify(messageData));
       }
     });
   });
